d8413e8af58575080be25e89f205388e7bab3663
Kimberly Zawacki <ximberlyzawacki@gmail.com>
2019-09-26 15:26:13 -0400

    chore: updated files to adhere to phaser 3

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
+import Phaser from 'phaser'
+
 export default {
-  gameWidth: 760,
-  gameHeight: 400,
-  localStorageName: 'phaseres6webpack',
-  webfonts: ['Bangers']
+  type: Phaser.AUTO,
+  parent: 'content',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  // width: 800,
+  // height: 600,
+  localStorageName: 'phaseres6webpack'
 }
---
+++ src/scenes/Game.js
@@ -0,0 +1,28 @@
+/* globals __DEV__ */
+import Phaser from 'phaser'
+
+import Mushroom from '../sprites/Mushroom'
+
+export default class extends Phaser.Scene {
+  constructor () {
+    super({ key: 'GameScene' })
+  }
+  init () {}
+  preload () {}
+
+  create () {
+    this.mushroom = new Mushroom({
+      scene: this,
+      x: 400,
+      y: 300,
+      asset: 'mushroom'
+    })
+
+    this.add.existing(this.mushroom)
+    this.add.text(400, 100, 'Chia ', {
+      font: '64px Bangers',
+      fill: '#7744ff'
+    })
+
+  }
+}
